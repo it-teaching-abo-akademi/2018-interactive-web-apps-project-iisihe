a24f91d4a581ec53e53a338c21f3be6ad3951ea9
Iina-Sisko Helander <iina-sisko.e.helander@utu.fi>
2018-12-20 17:39:10 +0200

    Updates

---
+++ precache-manifest.cce7f723ae424606a51258b79b962d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17e63e1b645000c0430a",
+    "url": "/2018-interactive-web-apps-project-iisihe/static/css/main.98712244.chunk.css"
+  },
+  {
+    "revision": "17e63e1b645000c0430a",
+    "url": "/2018-interactive-web-apps-project-iisihe/static/js/main.17e63e1b.chunk.js"
+  },
+  {
+    "revision": "345e0b1b8bf2a101b08e",
+    "url": "/2018-interactive-web-apps-project-iisihe/static/css/1.3bce757f.chunk.css"
+  },
+  {
+    "revision": "345e0b1b8bf2a101b08e",
+    "url": "/2018-interactive-web-apps-project-iisihe/static/js/1.345e0b1b.chunk.js"
+  },
+  {
+    "revision": "8874bd4c12ab673c8564",
+    "url": "/2018-interactive-web-apps-project-iisihe/static/js/runtime~main.8874bd4c.js"
+  },
+  {
+    "revision": "e410b2e25c045df52a5bfd31b9038b5d",
+    "url": "/2018-interactive-web-apps-project-iisihe/index.html"
+  }
+];
